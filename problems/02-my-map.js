748fbcfdc04b8944e99271d7016c823c94f75981
Renerose Dimatulac <rsdimatulac@gmail.com>
2021-01-21 17:30:31 -0600

    02 done again

--- problems/02-my-map.js
+++ problems/02-my-map.js
@@ -16,14 +16,24 @@ let result2 = myMap(['run', 'Forrest'], function (el) {
 console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 *******************************************************************************/
 
-let myMap = function() {
+let myMap = function(array, cb) {
 
+    let newArr = [];
+    for ( let i = 0; i < array.length; i++ ) {
+        let ele = array[i];
+        let res = cb(ele);
+        newArray.push(res);
+    };
+    return newArr;
 };
 
+let result1 = myMap([100, 25, 81, 64], Math.sqrt);
+console.log(result1);   // [ 10, 5, 9, 8 ]
 
-
-
-
+let result2 = myMap(['run', 'Forrest'], function (el) {
+    return el.toUpperCase() + '!';
+});
+console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 module.exports = myMap;
