9db149e966b89197385457965c6de22c13c8b117
Renerose Dimatulac <rsdimatulac@gmail.com>
2021-01-21 17:27:43 -0600

    03 done

--- problems/03-multi-map.js
+++ problems/03-multi-map.js
@@ -21,12 +21,28 @@ let result3 = multiMap("hi", 5, function(s) {
 console.log(result3); // hi!!!!!
 *******************************************************************************/
 
-let multiMap = function() {
+let multiMap = function(value, n, cb) {
 
+  for ( let i = 0; i < n; i++ ) {
+    value = cb(value);
+  }
+  return value;
 };
 
+let result1 = multiMap(7, 2, function (n) {
+  return n * 10;
+});
+console.log(result1); // 700
 
+let result2 = multiMap(7, 3, function (n) {
+  return n * 10;
+});
+console.log(result2); // 7000
 
+let result3 = multiMap("hi", 5, function (s) {
+  return s + "!";
+});
+console.log(result3); // hi!!!!!
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 module.exports = multiMap;
